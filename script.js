096f5bb233f9eebd68a4e268f3a58a6fc48e90e0
Bekam <mspbekan@gmail.com>
2023-03-18 22:03:58 -0500

    variables defined

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
+  lengthOfPass = prompt("enter the length of your passw");
+  var number = 0123456789;
+  var lowerCase= "abcdefghijklmnopqrstuvwxyz";
+  var upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var symbol= "/:;<=>?@[\]^_`{|}~";
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -10,5 +19,8 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
