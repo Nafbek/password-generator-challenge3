b704446273fc0252cf71635b20451d3d05c9f4b2
Bekam <mspbekan@gmail.com>
2023-03-19 15:21:55 -0500

    code to check the validity of user's input and to define variables added

--- script.js
+++ script.js
@@ -4,19 +4,36 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 // Write password to the #password input
 function writePassword() {
-  lengthOfPass = prompt("enter the length of your passw");
-  var number = 0123456789;
-  var lowerCase= "abcdefghijklmnopqrstuvwxyz";
-  var upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var symbol= "/:;<=>?@[\]^_`{|}~";
+  lengthOfPassword = prompt("Write the preferred length of your password.\n It should be at least 8 and atmost 128 characters.");
 
-  var password = generatePassword();
+  //check wether the user input is valid 
+if(lengthOfPassword >=8 && lengthOfPassword <= 128){
+  var numberChar = confirm('would you like to include lowercase?');
+  var upperCaseChar = confirm('would you like to include uppercase?');
+  var lowerCaseChar = confirm('would you like to include number?');
+  var symbolChar = confirm('would you like to include symbol?');
+} else{
+   lengthOfPassword = prompt("please try again, min 8 char");
+   writePassword();
+}
+//characters available for users to choose from
+ numberChar = "0123456789";
+lowerCaseChar= "abcdefghijklmnopqrstuvwxyz";
+ upperCaseChar= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   symbolChar= "/:;<=>?@[\]^_`{|}~";
+
+
+  password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function generatePassword(numberChar, lowerCaseChar,  upperCaseChar, upperCaseChar,.........){
+  //should it take the lengthOFPassword as an argument or not?as it generates the password based on the length of password chosen by user?
 }
 
 
@@ -24,3 +41,8 @@ function writePassword() {
 
 
 
+
+
+
+
+ 
