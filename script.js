23fe4853541a359a0a840e5cec9d23d61d0b8a14
Bekam <mspbekan@gmail.com>
2023-03-21 12:21:42 -0500

    variables redefined, the 'for' loop added, parameters placed inside the function

--- script.js
+++ script.js
@@ -4,36 +4,48 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//characters available for users to choose from
+
+// I did put these variables inside the writePassword function, I doubted its accessibility to the generetePassword function????????????????????
+
+var numberChar = "0123456789";
+var lowerCaseChar= "abcdefghijklmnopqrstuvwxyz";
+var upperCaseChar= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var symbolChar= "/:;<=>?@[\]^_`{|}~";
+
 
 // Write password to the #password input
 function writePassword() {
-  lengthOfPassword = prompt("Write the preferred length of your password.\n It should be at least 8 and atmost 128 characters.");
+  lengthOfPassword = prompt("Write the preferred length of your password.\nIt should be at least 8 and atmost 128 characters.");
 
   //check wether the user input is valid 
 if(lengthOfPassword >=8 && lengthOfPassword <= 128){
-  var numberChar = confirm('would you like to include lowercase?');
-  var upperCaseChar = confirm('would you like to include uppercase?');
-  var lowerCaseChar = confirm('would you like to include number?');
-  var symbolChar = confirm('would you like to include symbol?');
+
+  var iLikeNumber = confirm('would you like to include number?');
+  var ILikeLowerCase = confirm('would you like to include lowercase?');
+  var iLikeUpperCase = confirm('would you like to include uppercase?');
+  var iLikeSymbolChar = confirm('would you like to include symbol?');
+
+var password = generatePassword(lengthOfPassword, iLikeNumber, ILikeLowerCase, iLikeUpperCase, iLikeSymbolChar);
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  
 } else{
    lengthOfPassword = prompt("please try again, min 8 char");
    writePassword();
 }
-//characters available for users to choose from
- numberChar = "0123456789";
-lowerCaseChar= "abcdefghijklmnopqrstuvwxyz";
- upperCaseChar= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-   symbolChar= "/:;<=>?@[\]^_`{|}~";
-
-
-  password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
 }
 
-function generatePassword(numberChar, lowerCaseChar,  upperCaseChar, upperCaseChar,.........){
-  //should it take the lengthOFPassword as an argument or not?as it generates the password based on the length of password chosen by user?
+function generatePassword(length, iLikeNumber, ILikeLowerCase, iLikeUpperCase, iLikeSymbolChar){
+
+  var chosenChars = numberChar + lowerCaseChar + upperCaseChar + symbolChar; //disregard the user's preference. what type of condition should i use?
+  var password = "";
+  for (var i = 0; i< length; i++){
+    charIndex = Math.floor(Math.random()* chosenChars.length);
+    password += chosenChars.charAt(charIndex);
+  }
+return password;
 }
 
 
