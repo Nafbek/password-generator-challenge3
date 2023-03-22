fbbe8889377d3f9ce6b2e533af4bbcf3bf8f1f24
Bekam <mspbekan@gmail.com>
2023-03-22 16:01:08 -0500

    Logic added to check the inclusion of at least one character type, and README file improved

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# password-generator-challenge3
-This is a repo for password generator
+# password Generator
+
+## Description
+
+The aim of this project is to generate a random password for users based on the character type they choose. Four general character types are defined to generate a password from. ...........
+
+
+## User Story
+
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security.
+
+
+## Acceptance Criteria 
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page.
+
+## Installation
+
+You can clone this particular repository (follow this link  https://github.com/Nafbek/password-generator-challenge3 ) form GitHub, and paste it to your local machine. Unzip the repository directory, then open the 'index.html' file in any of your prefered web browser. 
+
+## Usage
+
+After you have opened the 'index.html' file in your prefered browser, click the 'Generator Password' button to begin generating a password. You will then be prompted to chose the length of the password (with a minimum 8 and maximum 128 characters) and the character types you would like to include. If the requirements are fulfilled, the computer will generate a password and display it inside the 'Generate Password' box.
+
+
+The mock-up of the page can be viewed either by double clicking on the file "Animation.gif" in the repository or by clicking on the follwing syntax link.
+
+
+
+![alt text](Animation.gif)
+
+
+## Credits
+
+I would like to appreciate Philip Clark, a Teaching Assistant, for his constructive feedback and suggestions. Particularly, he recommended using different variable names for the characters type used to generate a random password and the user's preference. In addition to this, he advised me to apply logic that  ensures at least one user's preference is included.  
+
+Overall, I am often grateful to open source, https://www.w3schools.com/js/js_function_definition.asp . 
+
+## License
+
+The MIT License applies (check here https://github.com/Nafbek/password-generator-challenge3/blob/main/LICENSE for details).
--- script.js
+++ script.js
@@ -4,48 +4,61 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//characters available for users to choose from
-
-// I did put these variables inside the writePassword function, I doubted its accessibility to the generetePassword function????????????????????
-
+//Type of characters used to generate password
 var numberChar = "0123456789";
 var lowerCaseChar= "abcdefghijklmnopqrstuvwxyz";
 var upperCaseChar= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var symbolChar= "/:;<=>?@[\]^_`{|}~";
 
-
 // Write password to the #password input
 function writePassword() {
   lengthOfPassword = prompt("Write the preferred length of your password.\nIt should be at least 8 and atmost 128 characters.");
 
-  //check wether the user input is valid 
+  //Check whether the user input is valid 
 if(lengthOfPassword >=8 && lengthOfPassword <= 128){
-
   var iLikeNumber = confirm('would you like to include number?');
   var ILikeLowerCase = confirm('would you like to include lowercase?');
   var iLikeUpperCase = confirm('would you like to include uppercase?');
   var iLikeSymbolChar = confirm('would you like to include symbol?');
 
 var password = generatePassword(lengthOfPassword, iLikeNumber, ILikeLowerCase, iLikeUpperCase, iLikeSymbolChar);
-  var passwordText = document.querySelector("#password");
+var passwordText = document.querySelector("#password");
+passwordText.value = password;
 
-  passwordText.value = password;
-  
 } else{
-   lengthOfPassword = prompt("please try again, min 8 char");
-   writePassword();
+   alert("please try again, at least 8 characters");
+ }
 }
-}
-
+//why the parameters appear as their values not read????
 function generatePassword(length, iLikeNumber, ILikeLowerCase, iLikeUpperCase, iLikeSymbolChar){
-
-  var chosenChars = numberChar + lowerCaseChar + upperCaseChar + symbolChar; //disregard the user's preference. what type of condition should i use?
-  var password = "";
-  for (var i = 0; i< length; i++){
-    charIndex = Math.floor(Math.random()* chosenChars.length);
-    password += chosenChars.charAt(charIndex);
+  var chosenChars ="";
+  if (iLikeNumber){
+    chosenChars +=numberChar;
+    //what if i 
   }
-return password;
+  if (ILikeLowerCase){
+    chosenChars +=lowerCaseChar;
+  }
+  if (iLikeUpperCase){
+    chosenChars +=upperCaseChar;
+  }
+  if (iLikeSymbolChar){
+    chosenChars +=symbolChar;
+  }
+  // Check if at least one  is included??// but no effect, i think?
+  if(!iLikeNumber && !ILikeLowerCase && !iLikeUpperCase && !iLikeSymbolChar){
+    alert("Please chose at least one character type");
+    writePassword();
+  }
+// a code to ensure that a computer included at least one user's character type preference 
+
+  //Generate a random password
+  var Password = "";
+  for (var i = 0; i<length; i++){
+    var charIndex = Math.floor(Math.random()* chosenChars.length);
+    Password += chosenChars.charAt(charIndex);
+  }
+return Password;
 }
 
 
