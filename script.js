d0444de4d848c425cde7eafce9a62d4074e1d9af
Bekam <mspbekan@gmail.com>
2023-03-23 09:38:48 -0500

    a code that checks the inclusion of at least one character type is modified

--- script.js
+++ script.js
@@ -4,61 +4,69 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Type of characters used to generate password
-var numberChar = "0123456789";
-var lowerCaseChar= "abcdefghijklmnopqrstuvwxyz";
-var upperCaseChar= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var symbolChar= "/:;<=>?@[\]^_`{|}~";
-
-// Write password to the #password input
+// Write password to the #password input 
 function writePassword() {
   lengthOfPassword = prompt("Write the preferred length of your password.\nIt should be at least 8 and atmost 128 characters.");
 
-  //Check whether the user input is valid 
-if(lengthOfPassword >=8 && lengthOfPassword <= 128){
-  var iLikeNumber = confirm('would you like to include number?');
-  var ILikeLowerCase = confirm('would you like to include lowercase?');
-  var iLikeUpperCase = confirm('would you like to include uppercase?');
-  var iLikeSymbolChar = confirm('would you like to include symbol?');
+  //Check whether the user input is valid and prompt user to choose their prefered character type
+  if(lengthOfPassword >=8 && lengthOfPassword <= 128){
+    var iLikeNumber = confirm('would you like to include number?');
+    var iLikeLowerCase = confirm('would you like to include lowercase?');
+    var iLikeUpperCase = confirm('would you like to include uppercase?');
+    var iLikeSymbolChar = confirm('would you like to include symbol?');
 
-var password = generatePassword(lengthOfPassword, iLikeNumber, ILikeLowerCase, iLikeUpperCase, iLikeSymbolChar);
-var passwordText = document.querySelector("#password");
-passwordText.value = password;
+    var password = generatePassword(lengthOfPassword, iLikeNumber, iLikeLowerCase, iLikeUpperCase, iLikeSymbolChar);
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 
-} else{
-   alert("please try again, at least 8 characters");
+  } else{
+   alert("please try again, enter at least 8 characters");
  }
 }
-//why the parameters appear as their values not read????
-function generatePassword(length, iLikeNumber, ILikeLowerCase, iLikeUpperCase, iLikeSymbolChar){
+//
+function generatePassword(length, iLikeNumber, iLikeLowerCase, iLikeUpperCase, iLikeSymbolChar){
+  //Type of characters used to generate password
+  var numberChar = "0123456789";
+  var lowerCaseChar= "abcdefghijklmnopqrstuvwxyz";
+  var upperCaseChar= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var symbolChar= "/:;<=>?@[\]^_`{|}~";
+  
+  //Declare variables to store the chosen character types and the inclusion of at least one character type
   var chosenChars ="";
+  var charInclusion = false;
+
+  //Check user's preference of character type and 
   if (iLikeNumber){
     chosenChars +=numberChar;
-    //what if i 
+    charInclusion = true;
   }
-  if (ILikeLowerCase){
+  if (iLikeLowerCase){
     chosenChars +=lowerCaseChar;
+    charInclusion = true;
   }
   if (iLikeUpperCase){
     chosenChars +=upperCaseChar;
+    charInclusion = true;
   }
   if (iLikeSymbolChar){
     chosenChars +=symbolChar;
+    charInclusion = true;
   }
-  // Check if at least one  is included??// but no effect, i think?
-  if(!iLikeNumber && !ILikeLowerCase && !iLikeUpperCase && !iLikeSymbolChar){
-    alert("Please chose at least one character type");
-    writePassword();
-  }
-// a code to ensure that a computer included at least one user's character type preference 
-
+  
+  // Check if at least one character type from user's prefernce is included in the password
+  if ((!iLikeNumber && !iLikeLowerCase && !iLikeUpperCase && !iLikeSymbolChar) && (!charInclusion)){
+    alert("Please choose at least one character type");
+    writePassword();  //why no effect?
+  }else{
   //Generate a random password
   var Password = "";
   for (var i = 0; i<length; i++){
     var charIndex = Math.floor(Math.random()* chosenChars.length);
     Password += chosenChars.charAt(charIndex);
   }
-return Password;
+   return Password;
+  }
+  
 }
 
 
